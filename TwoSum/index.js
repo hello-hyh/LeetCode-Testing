f68b07e829c184bdbf059a17fa94a445a4402294
hellohyh <15055667366@qq.com>
2020-05-18 21:53:26 +0800

    added: add twosum code.

---
+++ TwoSum/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let len = nums.length
+  let secondNumTmp = 0
+  let result = []
+  if (len <= 2 && nums[0] + nums[1] === target) return [0, 1]
+  for (let i = 0; i < len; i++) {
+    secondNumTmp = target - nums[i]
+    if (result[secondNumTmp] !== undefined) return [result[secondNumTmp], i]
+    result[nums[i]] = i
+  }
+};
